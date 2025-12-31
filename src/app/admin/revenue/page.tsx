a8d58139df766cbfe0c9f19daa337725c70b1326
Irorunoluwa Akinyemi <roarnml@gmail.com>
2025-12-31 12:55:44 +0100

    Update for staging deployment

--- src/app/admin/revenue/page.tsx
+++ src/app/admin/revenue/page.tsx
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma"
 import RevenueChart from "./revenue-chart"
 import ExportCSVButton from "./export-csv"
-import { PrismaClient } from "@prisma/client"
+//import { PrismaClient } from "@prisma/client"
 
 
 function startOfMonth(date: Date) {
